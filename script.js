040b6bcfab3f155863a339b551a21f088e15ae7c
truds99 <dudscardoso9@gmail.com>
2023-12-08 17:12:47 -0300

    ready project

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const numberCards = prompt ("Pick a number (pair) from 4 to 14 cards to play");
 const images = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
 images.sort(compare);
 const deck = [];
-let cont = 0, card1, card2;
+let count = 0, countPlays = 0, hits = 0, card1, card2;
+let numberCards = prompt ("Pick a number (pair) from 4 to 14 cards to play");
+
+while (numberCards < 4 || numberCards > 14 || numberCards % 2 != 0){
+        numberCards = prompt ("Pick a number (pair) from 4 to 14 cards to play");
+    }
+
+renderDeck();    
 
 function compare() { 
 	return Math.random() - 0.5; 
@@ -44,27 +50,39 @@ function renderDeck(){
     }
 }
 
-renderDeck();
-
 function flipCard (card){
-    if (cont === 0) {
-        card.classList.add("frontFace", "card1");
-        card1 = card;
-        cont = 1;
-    } else { 
-        card.classList.add("frontFace", "card2");
-        card2 = card;
-            if (card1.querySelector(".card-content").querySelector(".card-front").querySelector("img").src === card2.querySelector(".card-content").querySelector(".card-front").querySelector("img").src){
-                card1.classList.remove("card1");
-                card2.classList.remove("card2");
-            } else {
-                setTimeout(unflipCards, 1000);
+    if (count != 2) {
+        if (count === 0) {
+            card.classList.add("frontFace", "card1");
+            card1 = card;
+            count = 1;
+        }   else { 
+                card.classList.add("frontFace", "card2");
+                card2 = card;
+                count = 2;
+                if (card1.querySelector(".card-content").querySelector(".card-front").querySelector("img").src === card2.querySelector(".card-content").querySelector(".card-front").querySelector("img").src){
+                    card1.classList.remove("card1");
+                    card2.classList.remove("card2");
+                    count = 0;
+                    hits ++;
+                    if (hits === numberCards/2) {
+                        countPlays ++;
+                        setTimeout(msg, 650);   
+                    }
+                }   else {
+                        setTimeout(unflipCards, 1000);
+                    }
             }
-            cont = 0;
+        countPlays ++;    
         }
     }
 
-function unflipCards (){
-    document.querySelector(".card1").classList.remove("frontFace", "card1");
-    document.querySelector(".card2").classList.remove("frontFace", "card2");
+function unflipCards(){
+    card1.classList.remove("frontFace", "card1");
+    card2.classList.remove("frontFace", "card2");
+    count = 0;
+}
+
+function msg() {
+    alert (`Você ganhou em ${countPlays} jogadas`);
 }
